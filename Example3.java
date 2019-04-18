f1d1f1acecde8b56e731b47951828914969841ed
Ridanazir123 <49753338+Ridanazir123@users.noreply.github.com>
2019-04-18 16:49:43 +0500

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,17 @@
+class Example3 {
+ public static void main(String args[]) {
+ int var;
+ double x;
+ var = 10;
+ x = 10.0;
+ System.out.println(" Original value of var:" + var);
+ System.out.println(" Original value of x:" + x);
+ System.out.println();
+ var = var / 4;
+ x = x / 4;
+ System.out.println(" var after devision :" + var);
+ System.out.println(" x after devision :" + x);
+ }
+}
+
+
