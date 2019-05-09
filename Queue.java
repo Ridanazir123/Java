284a093a517ad5752b3aabe1b4f80d7acdf29910
Ridanazir123 <49753338+Ridanazir123@users.noreply.github.com>
2019-05-09 11:28:08 +0500

    queue

--- Queue.java
+++ Queue.java
@@ -1,39 +1,25 @@
 class Queue {
- char q[];
- int putloc,getloc;
- Queue(int size) {
-  q = new char[size];
-  putloc = getloc = 0;
- }
- void put(char ch) {
-  if (putloc == q.length) {
-   System.out.println(" - Queue is full");
-   return;
-  }
-   q[putloc++] = ch;
- }
- char get() {
-  if (getloc == putloc) {
-    System.out.println("Queue is empty"); 
-    return (char) 0;
-  }
-  return q[getloc++];
- }
+public static void main(String args[]) {
+ char q[] ;
+int putloc ,getloc;
+Queue(int size) {
+ q = new char [size];
+putloc = getloc = 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void put(char ch) {
+if(putloc == q.length) {
+System.out.println(" - queue is full.");
+return;
+} 
+q[putloc++] = ch;
+}
+char get() {
+if(getloc == putloc) {
+System.out.println(" queue iss empty");
+return (char) 0;
+}
+return q[getloc++] ;
+}
+} 
+}
 
